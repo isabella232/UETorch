0e01f28d8e8eb72670f59a220ee37729320f1cb7
Adam Lerer <adam.lerer@gmail.com>
2016-02-21 19:16:40 -0500

    setup

--- Setup.sh
+++ Setup.sh
@@ -3,13 +3,19 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Setting up Lua..."
+echo "=== Checking out the baseline UE4 commit... ==="
+git checkout 4.8
+#git checkout c4001bc95e3e6490c31f0d8dd0699ea2f22e3661
+git checkout 648eec4314879fc6e2d7aea767fade0f9e446c36
+
+echo "=== Patching UE4 ==="
+cd $DIR/../../..
+git apply $DIR/UnrealEngine.patch
+
+echo "=== Setting up Lua... === "
 cd $DIR/../ScriptPlugin/Source/Lua
 curl -R -O http://www.lua.org/ftp/lua-5.2.4.tar.gz
 tar zxf lua-5.2.4.tar.gz
 cd lua-5.2.4
 make CFLAGS='-fPIC' linux test
 
-echo "Patching UnrealEngine"
-cd $DIR/../../..
-git apply $DIR/UnrealEngine.patch
